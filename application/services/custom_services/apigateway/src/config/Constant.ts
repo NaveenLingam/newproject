15d33b4973339c4fb39f4a41d42bd9d556ae80a5
NaveenLingam <naveenlingam@10decoders.in>
2020-05-29 08:11:20 +0000

    code commited from geppetto!

---
+++ application/services/custom_services/apigateway/src/config/Constant.ts
@@ -0,0 +1,4 @@
+export const SECURITYURL = 'http://newproject-app.newproject.svc.cluster.local:3007';
+export const AUTHPROXYURL = 'http://newproject-app.newproject.svc.cluster.local:3009';
+export const ADMINURL = 'http://newproject-app.newproject.svc.cluster.local:3010';
+export const CAMUNDAURL = 'http://newproject-app.newproject.svc.cluster.local:3008';
